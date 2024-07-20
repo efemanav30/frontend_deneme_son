e3ef15772bc959cfd9103a3ad35a35afa27b0bb9
Süleyman Efe Manav <efemanav30@gmail.com>
2024-07-21 02:55:37 +0300

    koordinat ekleme tamam

--- src/app/mainPage/table-list/add/add.component.ts
+++ src/app/mainPage/table-list/add/add.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IlService } from '../../services/il.service';
 import { IlceService } from '../../services/ilce.service';
 import { TasinmazService } from 'src/app/tasinmaz.service';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Tasinmaz } from 'src/app/models/tasinmaz';
 import { Il } from 'src/app/models/il';
 import { Ilce } from 'src/app/models/ilce';
 import { Mahalle } from 'src/app/models/mahalle';
 import { MahalleService } from '../../services/mahalle.service';
-
 import { Router } from "@angular/router";
-
-
+import { Map, View } from 'ol';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
+import { Coordinate } from 'ol/coordinate';
 
 @Component({
   selector: 'app-add',
@@ -20,7 +27,9 @@ import { Router } from "@angular/router";
   providers: [TasinmazService],
 })
 export class AddComponent {
-
+  showMap = false;
+  selectedCoordinate: { lon: number, lat: number } | null = null;
+  @ViewChild('mapContainer') mapContainer: ElementRef;
   newTasinmaz: Tasinmaz;
   mahalleler: Mahalle[] = [];
   iller: Il[] = [];
@@ -30,8 +39,16 @@ export class AddComponent {
   selectedIlce: number;
   selectedMahalle: number;
   showSuccessAlert: boolean = false;
-  
+  map: any;
+  vectorSource: any;
 
+  constructor(private ilService: IlService, private ilceService: IlceService, private tasinmazService: TasinmazService, private formBuilder: FormBuilder, private mahalleService: MahalleService, private router: Router) {
+    this.createTasinmazForm();
+  }
+
+  ngOnInit() {
+    this.loadIller();
+  }
 
   createTasinmazForm() {
     this.tasinmazForm = this.formBuilder.group({
@@ -43,22 +60,12 @@ export class AddComponent {
       nitelik: ['', Validators.required],
       koordinatBilgileri: ['', Validators.required],
       adres: ['', Validators.required],
-
     });
-  }
-  
-
-  constructor(private ilService: IlService, private ilceService: IlceService, private tasinmazService: TasinmazService, private formBuilder: FormBuilder, private mahalleService: MahalleService,   private router: Router,) { }
-
-  ngOnInit() {
-    this.loadIller();
-    this.createTasinmazForm();
   }
 
   loadIller() {
     this.ilService.getIller().subscribe((data) => {
       this.iller = data;
-      //sort the iller by alphabetical order
     }, (error) => {
       console.error('İller yüklenirken hata oluştu', error);
     });
@@ -69,15 +76,14 @@ export class AddComponent {
     this.loadIlceler(ilId);
   }
 
-
   loadIlceler(ilId: number): void {
     this.ilceService.getIlcelerByIlId(ilId).subscribe(data => {
       this.ilceler = data;
-
     }, error => {
       console.error('İlçeler yüklenirken hata oluştu', error);
     });
   }
+
   onIlceChange(ilceId: number): void {
     this.selectedIlce = ilceId;
     this.loadMahalleler(ilceId);
@@ -86,59 +92,95 @@ export class AddComponent {
   loadMahalleler(ilceId: number): void {
     this.mahalleService.getMahallelerByIlceId(ilceId).subscribe(data => {
       this.mahalleler = data;
-      console.log('API Response mahalle:', data); // Gelen veriyi kontrol edin
-
+      console.log('API Response mahalle:', data);
     }, error => {
       console.error('Mahalleler yüklenirken hata oluştu', error);
     });
   }
 
-  /*add(): void {
-    if (this.tasinmazForm.valid) {
-      // Formdan gelen verileri newTasinmaz nesnesine atayın
-      this.newTasinmaz.il = parseInt(this.tasinmazForm.get("il").value);
-      this.newTasinmaz.ilce = parseInt(this.tasinmazForm.get("ilce").value);
-      this.newTasinmaz.mahalleId = parseInt(this.tasinmazForm.get("mahalleId").value);
-      this.newTasinmaz.ada = this.tasinmazForm.get("ada").value;
-      this.newTasinmaz.parsel = this.tasinmazForm.get("parsel").value;
-      this.newTasinmaz.nitelik = this.tasinmazForm.get("nitelik").value;
-      this.newTasinmaz.koordinatBilgileri = this.tasinmazForm.get("koordinatBilgileri").value;
-      
-  
-      // newTasinmaz nesnesini API'ye gönderin
-      this.tasinmazService.addTasinmaz(this.newTasinmaz).subscribe(response => {
-        console.log('Taşınmaz başarıyla eklendi');
-        // Başarılı ekleme sonrası yapılacak işlemler
-      }, error => {
-        console.error('Taşınmaz eklenirken bir hata oluştu', error);
+  openMap(): void {
+    this.showMap = true;
+    setTimeout(() => this.initializeMap(), 0);
+  }
+
+  initializeMap(): void {
+    if (this.map) {
+      this.map.setTarget(this.mapContainer.nativeElement);
+    } else {
+      this.vectorSource = new VectorSource();
+      this.map = new Map({
+        target: this.mapContainer.nativeElement,
+        layers: [
+          new Tile({
+            source: new OSM()
+          }),
+          new VectorLayer({
+            source: this.vectorSource,
+            style: new Style({
+              image: new CircleStyle({
+                radius: 7,
+                fill: new Fill({ color: 'red' }),
+                stroke: new Stroke({
+                  color: 'black',
+                  width: 2,
+                }),
+              }),
+            }),
+          }),
+        ],
+        view: new View({
+          center: fromLonLat([29.15, 36.6]), // Başlangıç merkezi
+          zoom: 14 // Başlangıç zoom seviyesi
+        })
+      });
+
+      this.map.on('click', (event) => {
+        const coords = toLonLat(event.coordinate);
+        this.onCoordinateSelected([coords[1], coords[0]]); // Enlem ve boylamı doğru sırayla kullanıyoruz
       });
     }
+    this.mapContainer.nativeElement.style.display = 'block';
+    this.map.updateSize(); // Harita boyutunu güncelle
   }
-  */
+
+  onCoordinateSelected(coords: Coordinate): void {
+    this.selectedCoordinate = {
+      lon: coords[1], // Lon ve Lat sıralamasını düzeltiyoruz
+      lat: coords[0],
+    };
+    this.tasinmazForm.patchValue({
+      koordinatBilgileri: `${this.selectedCoordinate.lat.toFixed(6)}, ${this.selectedCoordinate.lon.toFixed(6)}`
+    });
+
+    const feature = new Feature({
+      geometry: new Point(fromLonLat([this.selectedCoordinate.lon, this.selectedCoordinate.lat])),
+    });
+    this.vectorSource.clear();
+    this.vectorSource.addFeature(feature);
+
+    this.showMap = false;
+  }
+
   add(): void {
     if (this.tasinmazForm.valid) {
-      // Formdan gelen verileri newTasinmaz nesnesine atayın
-      // this.newTasinmaz.il = parseInt(this.tasinmazForm.get("il").value);
-      // this.newTasinmaz.ilce = parseInt(this.tasinmazForm.get("ilce").value);
-      // this.newTasinmaz.mahalleId = parseInt(this.tasinmazForm.get("mahalleId").value);
-      // this.newTasinmaz.ada = this.tasinmazForm.get("ada").value;
-      // this.newTasinmaz.parsel = this.tasinmazForm.get("parsel").value;
-      // this.newTasinmaz.nitelik = this.tasinmazForm.get("nitelik").value;
-      // this.newTasinmaz.koordinatBilgileri = this.tasinmazForm.get("koordinatBilgileri").value;
-      // this.newTasinmaz.adres = this.tasinmazForm.get("adres").value;
-      this.newTasinmaz = new Tasinmaz(parseInt(this.tasinmazForm.get("mahalleId").value),this.tasinmazForm.get("ada").value,this.tasinmazForm.get("parsel").value,this.tasinmazForm.get("nitelik").value,this.tasinmazForm.get("koordinatBilgileri").value,this.tasinmazForm.get("adres").value);
+      this.newTasinmaz = new Tasinmaz(
+        parseInt(this.tasinmazForm.get("mahalleId").value),
+        this.tasinmazForm.get("ada").value,
+        this.tasinmazForm.get("parsel").value,
+        this.tasinmazForm.get("nitelik").value,
+        this.tasinmazForm.get("koordinatBilgileri").value,
+        this.tasinmazForm.get("adres").value
+      );
   
-      console.log('New Tasinmaz:', this.newTasinmaz); // Gelen veriyi kontrol edin
+      console.log('New Tasinmaz:', this.newTasinmaz);
   
-      // newTasinmaz nesnesini API'ye gönderin
       this.tasinmazService.addTasinmaz(this.newTasinmaz).subscribe(response => {
         console.log('Taşınmaz başarıyla eklendi');
         location.reload();
-        this.router.navigate(['/table-list']); // Başarılı ekleme sonrası yapılacak işlemler
+        this.router.navigate(['/table-list']);
       }, error => {
         console.error('Taşınmaz eklenirken bir hata oluştu', error);
       });
     }
   }
-  
 }
--- src/app/mainPage/table-list/map/map.component.ts
+++ src/app/mainPage/table-list/map/map.component.ts
@@ -1,31 +1,151 @@
-import { Component, AfterViewInit } from '@angular/core';
-import 'ol/ol.css';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import * as ol from 'ol';
+
 import OSM from 'ol/source/OSM';
+import 'ol/ol.css';
+import { fromLonLat } from 'ol/proj';
+import { Coordinate } from 'ol/coordinate';
+import ScaleLine from 'ol/control/ScaleLine';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Point } from 'ol/geom';
+import { Feature } from 'ol';
+import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
+import { Tasinmaz } from 'src/app/models/tasinmaz';
+import TileImage from 'ol/source/TileImage';
+import { environment } from 'src/environments/environment';
+declare var google: any;
+
+
+
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements OnInit {
   map: Map;
+  osmLayer: TileLayer<any>;
+  osmLayerOpacity: number = 1;
+  @Output() coordinateClicked = new EventEmitter<Coordinate>();
+  vectorSource: VectorSource;
+  vectorLayer: VectorLayer<any>;
+  googleLayer: TileLayer<any>; 
+  showGoogleMap: boolean = false; 
+  mousemove: Coordinate; 
+  mousemoves: string;
+  mouseX: number;
+  showOpenStreetMap: boolean = false;
+  mouseY: number;
+  isMouseOver: boolean;
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-    this.map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM()
+  ngOnInit() {
+    this.vectorSource = new VectorSource();
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+      style: new Style({
+        image: new CircleStyle({
+          radius: 5,
+          fill: new Fill({color: 'red'}),
+          stroke: new Stroke({
+            color: [255,0,0], width: 2
+          })
         })
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 2
       })
     });
+   
+    this.osmLayer = new TileLayer({
+      source: new OSM(),
+      opacity: this.osmLayerOpacity
+    });
+
+    this.map = new Map({
+      target: 'map',
+      layers: [this.osmLayer, this.vectorLayer],
+      view: new View({
+        center: fromLonLat([35, 39]),
+        zoom: 6
+      })
+    });
+
+    let scale = new ScaleLine({
+      units: 'metric',
+      bar: true,
+      steps: 4,
+      text: true,
+    });
+    this.map.addControl(scale);
+    scale.setTarget('scale-bar');
+
   }
+
+  
+
+  
+
+/*
+  toggleGoogleMap() {
+    if (!this.showGoogleMap) {
+      if (!this.googleLayer) {
+        this.googleLayer = new TileLayer({
+          source: new ol.source.TileImage({
+            url: `https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&key=${environment.googleMapsApiKey}`,
+            attributions: '© Google'
+          })
+        });
+      }
+      this.map.addLayer(this.googleLayer);
+    } else {
+      this.map.removeLayer(this.googleLayer);
+    }
+  }
+  
+*/
+
+ 
+
+  coordinatedClicked() {
+    this.map.on('click', (evt) => {
+      const coordinate = evt.coordinate; 
+        this.vectorSource.clear();
+        this.addPoint(coordinate);
+        this.coordinateClicked.emit(coordinate); 
+        console.log(coordinate); 
+    });
+  }
+  
+  addPoint(coordinate: Coordinate) {
+    const point = new Point(coordinate);
+    const feature = new Feature(point); 
+    this.vectorSource.addFeature(feature);
+  }
+  
+
+
+  setTasinmazCoordinates(tasinmazlar: Tasinmaz[]) {
+    tasinmazlar.forEach(tasinmaz => {
+      const coordinate: Coordinate = [tasinmaz.koordinatBilgileri];
+      this.addPoint(coordinate);
+    });
+  }
+  popupPage(){
+    this.map.on('click', function(evt){
+      var feature1 = this.map.ForEachFeatureAtPixel(evt.pixel,
+        function(feature1){
+          return feature1;
+
+        })
+        console.log(feature1);
+        
+
+    })
+  }
+
+  
 }
